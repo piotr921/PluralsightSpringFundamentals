f66ec8bcb5e556949e879a16eaa0fd57fcef248b
piotr921 <piotr92345@o2.pl>
2016-12-29 22:11:51 +0100

    End course

--- spring_sample_java/src/main/java/AppConfig.java
+++ spring_sample_java/src/main/java/AppConfig.java
@@ -1,6 +1,8 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
@@ -9,7 +11,13 @@ import com.pluralsight.service.CustomerServiceImpl;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer(){
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	/*
 	@Bean(name = "customerService")
